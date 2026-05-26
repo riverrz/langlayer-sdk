43e24c4945e9ca92896fd407ee7444a2d9d405ac
riverrz <shivam123425@gmail.com>
2026-05-26 16:06:27 +0400

    Add vanilla ts example project

---
+++ apps/vanilla-ts-example/src/library/langlayer.ts
@@ -0,0 +1,9 @@
+import { createLangLayer } from "@langlayer/vanilla";
+
+const ll = createLangLayer({
+  organizationSlug: "langlayer-sdk-examples-OVXRVE",
+  projectSlug: "vanilla-ts-l1BqJs",
+  fallbackLanguage: "en",
+});
+
+export default ll;
